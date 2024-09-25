cacad2d35cdf873ace6f216510fb33656f4905f1
NicolasLfs <nicolaslopes03@hotmail.com>
2024-09-25 19:22:55 -0300

    Att_Types

--- types.d.ts
+++ types.d.ts
@@ -1,19 +1,10 @@
 declare module "next-auth" {
-  /**
-   * The shape of the user object returned in the OAuth providers' `profile` callback,
-   * or the second parameter of the `session` callback, when using a database.
-   */
-  interface User {}
-  /**
-   * The shape of the account object returned in the OAuth providers' `account` callback,
-   * Usually contains information about the provider being used, like OAuth tokens (`access_token`, etc).
-   */
-  interface Account {}
-
-  /**
-   * Returned by `useSession`, `auth`, contains information about the active session.
-   */
-  interface Session {}
+  interface auth {
+    auth?: string;
+  }
+  interface nextUrl {
+    nextUrl?: string;
+  }
 }
 
 // The `JWT` interface can be found in the `next-auth/jwt` submodule
