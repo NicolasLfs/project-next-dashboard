5e1d2c6c7fc40b1505b6834c57e886931a9ff818
NicolasLfs <nicolaslopes03@hotmail.com>
2024-09-26 23:05:27 -0300

    Att_Login_v1.6

--- middleware.ts
+++ middleware.ts
@@ -3,12 +3,18 @@ import { authConfig } from "./auth.config";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-// This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL("/dashboard", request.url));
-}
-
 export default NextAuth(authConfig).auth;
+
+// Função de middleware
+export function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname.startsWith("/about")) {
+    return NextResponse.rewrite(new URL("/about-2", request.url));
+  }
+
+  if (request.nextUrl.pathname.startsWith("/dashboard")) {
+    return NextResponse.rewrite(new URL("/dashboard/user", request.url));
+  }
+}
 
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
